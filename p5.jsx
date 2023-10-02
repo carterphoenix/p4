a2c39114dca881732c3c45c5e5730bdc18549d33
carterphoenix <143209429+carterphoenix@users.noreply.github.com>
2023-10-02 17:27:29 -0400

    Update p5.jsx

--- p5.jsx
+++ p5.jsx
@@ -6,6 +6,10 @@ import Header from './components/header/Header';
 import Example from './components/example/Example';
 import States from './components/states/States';
 
+ReactDOM.render(
+    <Header/>,
+    document.getElementById('reactappHeader')
+);
 
 ReactDOM.render(
     <HashRouter>
