1be2274a39a116cc172adb6d9d333650dada8d54
carterphoenix <143209429+carterphoenix@users.noreply.github.com>
2023-10-02 17:27:04 -0400

    Update p4.jsx

--- p4.jsx
+++ p4.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.css';
 
+import Header from './components/header/Header';
 import Example from './components/example/Example';
 import States from './components/states/States';
 
+ReactDOM.render(
+    <Header/>,
+    document.getElementById('reactappHeader')
+);
 
 class Toggle extends React.Component {
     constructor(props) {
