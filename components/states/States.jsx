f5807a9e1c304d32f49825dac14871d34601c76f
carterphoenix <143209429+carterphoenix@users.noreply.github.com>
2023-10-02 17:23:52 -0400

    Update States.jsx

--- components/states/States.jsx
+++ components/states/States.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import './States.css';
 
-/**
- * Define States, a React component of Project 4, Problem 2. The model
- * data for this view (the state names) is available at
- * window.models.statesModel().
- */
+
 class States extends React.Component {
   constructor(props) {
     super(props);
