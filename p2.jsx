89f8d375934a0c5b75475ba0e4f31e1b3bb9a3a7
carterphoenix <143209429+carterphoenix@users.noreply.github.com>
2023-10-02 17:26:24 -0400

    Update p2.jsx

--- p2.jsx
+++ p2.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from './components/header/Header';
 import States from './components/states/States';
+
+ReactDOM.render(
+  <Header />,
+  document.getElementById('reactappHeader')
+);
 
 ReactDOM.render(
   <States />,
