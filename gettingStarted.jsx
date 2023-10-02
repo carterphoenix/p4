baf0e8c97cdbe1db41715cab3dbd1ae07fd23153
carterphoenix <143209429+carterphoenix@users.noreply.github.com>
2023-10-02 17:25:43 -0400

    Update gettingStarted.jsx

--- gettingStarted.jsx
+++ gettingStarted.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.css';
 
+import Header from './components/header/Header';
 import Example from './components/example/Example';
+
+ReactDOM.render(
+  <Header/>,
+  document.getElementById('reactappHeader')
+);
 
 ReactDOM.render(
   <Example />,
